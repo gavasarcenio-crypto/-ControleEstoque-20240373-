729fa6cd7f5af4c7fb7d38e32564046096241d56
gavasarcenio-crypto <ga.vas.arcenio@gmail.com>
2025-12-04 20:31:04 -0300

    Add method to find Estoque by produtoId

--- ControleEstoque-20240373/controle-estoque/src/main/java/com/myproject/controle_estoque/repository/EstoqueRepository.java
+++ ControleEstoque-20240373/controle-estoque/src/main/java/com/myproject/controle_estoque/repository/EstoqueRepository.java
@@ -3,4 +3,6 @@ package com.myproject.controle_estoque.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.myproject.controle_estoque.model.Estoque;
 
-public interface EstoqueRepository extends JpaRepository<Estoque, Long> {}
+public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
+
+    Estoque findByProdutoId(Long produtoId);}
